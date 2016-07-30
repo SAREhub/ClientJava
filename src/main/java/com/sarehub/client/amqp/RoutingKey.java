065f6ca8bbee77d2ed540139b7f8f2b800bd6408
Mararok <mararok@gmail.com>
2016-07-30 12:22:27 +0200

    moved Routingkey to amqp package

---
+++ src/main/java/com/sarehub/client/amqp/RoutingKey.java
@@ -0,0 +1,72 @@
+package com.sarehub.client.amqp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringJoiner;
+
+/**
+ * Routing key is string in format: part1[.partN]
+ */
+public class RoutingKey implements Iterable<String> {
+	
+	private List<String> parts;
+	
+	public RoutingKey() {
+		 parts = new ArrayList<String>();
+	}
+	
+	/**
+	 * @param parts
+	 */
+	public RoutingKey(List<String> parts) {
+		 this.parts = parts;
+	}
+	
+	public RoutingKey(String[] parts) {
+		this(new ArrayList<String>(Arrays.asList(parts)));
+	}
+	
+	/**
+	 * @param part
+	 */
+	public RoutingKey addPart(String part) {
+		parts.add(part);
+		return this;
+	}
+	
+	/**
+	 * @param index
+	 */
+	public String getPart(int index) {
+		return parts.get(index);
+	}
+	
+	public String[] getParts() {
+		return parts.toArray(new String[parts.size()]);
+	}
+	
+	public boolean isEmpty() {
+		return parts.isEmpty();
+	}
+		
+	@Override
+	public Iterator<String> iterator() {
+		return parts.iterator();
+	}
+	
+	@Override
+	public String toString() {
+		StringJoiner joiner = new StringJoiner(".");
+		for (String part : parts) {
+			joiner.add(part);
+		}
+		
+		return joiner.toString();
+	}
+
+	
+	
+	
+}
