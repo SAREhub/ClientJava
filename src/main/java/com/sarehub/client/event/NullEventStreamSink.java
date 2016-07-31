cc9bc915a41924ddc513df84c84455e21166a10e
Mararok <mararok@gmail.com>
2016-07-31 14:37:09 +0200

    reimplemented with interface

--- src/main/java/com/sarehub/client/event/NullEventStreamSink.java
+++ src/main/java/com/sarehub/client/event/NullEventStreamSink.java
@@ -3,10 +3,20 @@ package com.sarehub.client.event;
 /**
  * That implementation of EventStreamSink is used when sink for EventStreamSource isn't defined
  */
-public class NullEventStreamSink<E extends Event> extends EventStreamSink<E> {
+public class NullEventStreamSink<E extends Event> implements EventStreamSink<E> {
 
 	@Override
 	public void write(EventEnvelope<E> eventEnvelope) {
+
+	}
+
+	@Override
+	public void onPipe(EventStreamSource<E> source) {
+
+	}
+
+	@Override
+	public void onUnpipe(EventStreamSource<E> source) {
 
 	}
 
