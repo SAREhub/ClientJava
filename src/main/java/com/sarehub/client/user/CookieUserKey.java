21e875d1e746079f5f52985970f95ca30977255d
Mararok <mararok@gmail.com>
2016-08-01 07:10:29 +0200

    changed getKeyType of CookieUserKey to return "cookie" not "Cookie"

--- src/main/java/com/sarehub/client/user/CookieUserKey.java
+++ src/main/java/com/sarehub/client/user/CookieUserKey.java
@@ -4,21 +4,20 @@ package com.sarehub.client.user;
  * Simple user key from cookie value
  */
 public class CookieUserKey implements UserKey {
-	
+
 	private String cookie;
-	
+
 	public CookieUserKey(String cookie) {
 		this.cookie = cookie;
 	}
-	
+
 	public String getCookie() {
 		return cookie;
 	}
-	
+
 	@Override
 	public String getKeyType() {
-		return "Cookie";
+		return "cookie";
 	}
-
 
 }
