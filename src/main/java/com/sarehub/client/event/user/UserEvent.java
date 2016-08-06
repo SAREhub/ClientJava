6d39f06133f5fd223e6b7afcefb64a608e24d179
Mararok <mararok@gmail.com>
2016-08-06 11:24:32 +0200

    added docs

--- src/main/java/com/sarehub/client/event/user/UserEvent.java
+++ src/main/java/com/sarehub/client/event/user/UserEvent.java
@@ -3,6 +3,9 @@ package com.sarehub.client.event.user;
 import com.sarehub.client.event.Event;
 import com.sarehub.client.user.User;
 
+/**
+ * Base class for user events
+ */
 public abstract class UserEvent extends Event {
 
 	private User user;
