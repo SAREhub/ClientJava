c97f5768c87b17c64e2c1ea1fa8e11ad67d947dd
Mararok <mararok@gmail.com>
2016-09-04 14:13:03 +0200

    added BasicAmqpEventStreamSource

--- src/main/java/com/sarehub/client/amqp/BasicAmqpEventStreamSource.java
+++ src/main/java/com/sarehub/client/amqp/BasicAmqpEventStreamSource.java
@@ -1,5 +1,8 @@
 package com.sarehub.client.amqp;
 
+import static org.apache.commons.lang3.Validate.notBlank;
+import static org.apache.commons.lang3.Validate.notNull;
+
 import java.io.IOException;
 
 import com.rabbitmq.client.Channel;
@@ -8,55 +11,90 @@ import com.rabbitmq.client.QueueingConsumer;
 import com.rabbitmq.client.QueueingConsumer.Delivery;
 import com.rabbitmq.client.ShutdownSignalException;
 import com.sarehub.client.event.EventEnvelope;
-import com.sarehub.client.event.EventStreamSourceBase;
+import com.sarehub.client.event.EventStreamSink;
+import com.sarehub.client.event.EventStreamSource;
+import com.sarehub.client.event.NullEventStreamSink;
 
-public class BasicAmqpEventStreamSource extends EventStreamSourceBase {
+public class BasicAmqpEventStreamSource implements EventStreamSource {
 
-	private Channel channel;
-	private String queueName;
-	private DeliveryAmqpEventEnvelopeFactory eventEnvelopeFactory;
+	protected String queueName;
+	protected QueueingConsumer consumer;
+	protected String consumerTag;
 
-	private QueueingConsumer consumer;
-	private String consumerTag;
+	protected AmqpEventEnvelopeFactory eventEnvelopeFactory;
 
-	public BasicAmqpEventStreamSource(Channel channel, String queueName, DeliveryAmqpEventEnvelopeFactory eventEnvelopeFactory) {
-		this.channel = channel;
+	protected EventStreamSink sink;
+
+	protected boolean inFlowMode;
+
+	protected BasicAmqpEventStreamSource(String queueName) {
 		this.queueName = queueName;
-		this.eventEnvelopeFactory = eventEnvelopeFactory;
+		this.sink = new NullEventStreamSink();
 	}
 
-	public void initFlow() throws AmqpException {
-		if (isInFlowMode()) {
-			throw new AmqpException("That source is in flow mode");
+	public static BasicAmqpEventStreamSource getFor(String queueName) {
+		BasicAmqpEventStreamSource source = new BasicAmqpEventStreamSource(queueName);
+		return source;
+	}
+
+	public BasicAmqpEventStreamSource amqpConsumer(QueueingConsumer consumer) {
+		this.consumer = consumer;
+		return this;
+	}
+
+	public BasicAmqpEventStreamSource eventEnvelopeFactory(AmqpEventEnvelopeFactory eventEnvelopeFactory) {
+		this.eventEnvelopeFactory = eventEnvelopeFactory;
+		return this;
+	}
+
+	@Override
+	public void flow() throws Exception {
+		if (!isInFlowMode()) {
+			initFlow();
 		}
 
 		try {
-			consumer = new QueueingConsumer(channel);
-			consumerTag = channel.basicConsume(queueName, false, consumer);
-			channel.basicQos(2);
+			Delivery delivery = consumer.nextDelivery();
+			EventEnvelope eventEnvelope = eventEnvelopeFactory.createFromDelivery(delivery);
+			this.sink.write(eventEnvelope);
+		} catch (ShutdownSignalException | ConsumerCancelledException | InterruptedException e) {
+			throw new AmqpException("Error in source flow mode", e);
+		}
+	}
+
+	protected void initFlow() throws AmqpException {
+		try {
+			checkConfiguration();
+			getChannel().basicQos(2);
+			consumerTag = getChannel().basicConsume(queueName, false, consumer);
+			inFlowMode = true;
 		} catch (IOException e) {
 			throw new AmqpException("Can't init source flow mode", e);
 		}
 	}
 
-	@Override
-	public void flow() throws Exception {
-		if (isInFlowMode()) {
-			try {
-				Delivery delivery = consumer.nextDelivery();
-				EventEnvelope eventEnvelope = eventEnvelopeFactory.create(delivery);
-				this.sink.write(eventEnvelope);
-			} catch (ShutdownSignalException | ConsumerCancelledException | InterruptedException e) {
-				throw new AmqpException("Error in source flow mode", e);
-			}
-		}
+	protected void checkConfiguration() {
+		notNull(this.consumer, "AmqpConsumer can't be null");
+		notBlank(this.queueName, "Queue name cant't be blank");
+		notNull(this.eventEnvelopeFactory, "Event envelope factory can't be null");
+	}
+
+	public void pipe(EventStreamSink sink) {
+		this.sink.onUnpipe(this);
+		this.sink = sink;
+		this.sink.onPipe(this);
+	}
+
+	public void unpipe() {
+		this.sink.onUnpipe(this);
+		this.sink = new NullEventStreamSink();
 	}
 
 	public void stopFlow() throws AmqpException {
 		if (isInFlowMode()) {
 			try {
-				channel.basicCancel(consumerTag);
-				consumer = null;
+				getChannel().basicCancel(consumerTag);
+				inFlowMode = false;
 			} catch (IOException e) {
 				throw new AmqpException("Error when sending stop flow request to source", e);
 			}
@@ -64,11 +102,15 @@ public class BasicAmqpEventStreamSource extends EventStreamSourceBase {
 	}
 
 	public boolean isInFlowMode() {
-		return consumer != null;
+		return inFlowMode;
 	}
 
 	public String getQueueName() {
 		return queueName;
+	}
+
+	public Channel getChannel() {
+		return this.consumer.getChannel();
 	}
 
 }
